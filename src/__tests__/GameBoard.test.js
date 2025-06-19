297661b0b5016d667aabc5b2e193c143597bda21
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-19 15:03:30 +0530

    Enhance GameBoard class to validate ship placement and track occupied coordinates

--- src/__tests__/GameBoard.test.js
+++ src/__tests__/GameBoard.test.js
@@ -1,10 +1,32 @@
 import GameBoard from "../components/GameBoard";
 
 describe("GameBoard", () => {
+  //scope issues
   let gameBoard;
+  let mockShip;
 
   beforeEach(() => {
     gameBoard = new GameBoard();
+    mockShip = {
+      name: "Destroyer",
+      length: 3,
+      hits: 0,
+      sunk: false,
+      hit: jest.fn(),
+      isSunk: jest.fn().mockReturnValue(false),
+      getLength: jest.fn().mockReturnValue(3),
+      getHits: jest.fn().mockReturnValue(0),
+    };
+    mockShip2 = {
+      name: "Destroyer",
+      length: 4,
+      hits: 0,
+      sunk: false,
+      hit: jest.fn(),
+      isSunk: jest.fn().mockReturnValue(false),
+      getLength: jest.fn().mockReturnValue(4),
+      getHits: jest.fn().mockReturnValue(0),
+    };
   });
 
   it("should initialize with the correct dimensions", () => {
@@ -27,17 +49,17 @@ describe("GameBoard", () => {
   });
 
   it("should check if given coordinate for placing ship is valid", () => {
-    const mockShip = {
-      name: "Destroyer",
-      length: 3,
-      hits: 0,
-      sunk: false,
-      hit: jest.fn(),
-      isSunk: jest.fn().mockReturnValue(false),
-      getLength: jest.fn().mockReturnValue(3),
-      getHits: jest.fn().mockReturnValue(0),
-    };
     expect(gameBoard.placeShip(mockShip, [1, 3])).toBeTruthy();
     expect(gameBoard.placeShip(mockShip, [-1, 0])).toBeFalsy();
+  });
+
+  it("should check if ship fits for given coords", () => {
+    expect(gameBoard.placeShip(mockShip, [0, 7])).toBeTruthy();
+    expect(gameBoard.placeShip(mockShip, [0, 8])).toBeFalsy();
+  });
+
+  it("should not place a ship on same coords", () => {
+    expect(gameBoard.placeShip(mockShip, [1, 3])).toBeTruthy();
+    expect(gameBoard.placeShip(mockShip2, [1, 3])).toBeFalsy();
   });
 });
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -6,13 +6,32 @@ export default class GameBoard {
       Array(this.columns).fill(null)
     );
     this.ships = [];
+    this.coordHasShip = [];
+  }
+
+  #markAllCoords(x, y, shipLength) {
+    let i = 0;
+    while (i < shipLength) {
+      this.coordHasShip.push([x, y++]);
+      i++;
+    }
   }
 
   placeShip(ship, [x, y]) {
     const shipLength = ship.getLength();
-    if (x < 0 || x > 9 || y < 0 || y > 9) {
+    if (
+      x < 0 ||
+      x > 9 ||
+      y < 0 ||
+      y > 9 ||
+      y + shipLength - 1 > 9 ||
+      this.coordHasShip.some((coord) => coord[0] === x && coord[1] === y)
+    ) {
       return false;
     } else {
+      this.board[x][y] = ship;
+      this.ships.push(ship);
+      this.#markAllCoords(x, y, shipLength);
       return true;
     }
   }
