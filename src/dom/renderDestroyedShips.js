3cc8354e6048e33476054adc9a83cf810264083c
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-20 22:34:56 +0530

    Refactor renderDestroyedShips function to simplify logic and improve readability

--- src/dom/renderDestroyedShips.js
+++ src/dom/renderDestroyedShips.js
@@ -1,31 +1,23 @@
 export default function renderDestroyedShips(player) {
   const shipsData = player.gameBoard.ships;
-
   const currentPlayer = player.name;
 
-  function showSunkShip(sunkShipCoords) {
-    document
-      .querySelectorAll(`.board-${currentPlayer} .game-cell`)
-      .forEach((cell) => {
-        const row = parseInt(cell.dataset.row);
-        const column = parseInt(cell.dataset.column);
-
-        const isSunkShipCoord = sunkShipCoords.some((coord) => {
-          return coord[0] === row && coord[1] === column;
-        });
-
-        if (isSunkShipCoord) {
-          cell.classList.add("highlight-ship-sunk");
-        } else {
-          cell.classList.remove("highlight-ship-sunk");
-        }
-      });
-  }
+  document
+    .querySelectorAll(`.board-${currentPlayer} .game-cell`)
+    .forEach((cell) => cell.classList.remove("highlight-ship-sunk"));
 
   shipsData.forEach((shipDataObj) => {
     if (shipDataObj.ship.sunk) {
       const sunkShipCoords = shipDataObj.shipsCoords;
-      showSunkShip(sunkShipCoords);
+
+      sunkShipCoords.forEach(([row, column]) => {
+        const cell = document.querySelector(
+          `.board-${currentPlayer} .game-cell[data-row="${row}"][data-column="${column}"]`
+        );
+        if (cell) {
+          cell.classList.add("highlight-ship-sunk");
+        }
+      });
     }
   });
 }
