d64361a201b2c0c66c403ef71ba779ef6acdbc3d
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-22 11:49:44 +0530

    Rename addTempData to addEnemyData and refactor it to generate random data

---
+++ src/utils/addEnemyData.js
@@ -0,0 +1,29 @@
+import Ship from "../components/Ship";
+
+function getRandomCoord(shipLength) {
+  const x = Math.floor(Math.random() * 10);
+  const maxStartCol = 10 - shipLength;
+  const y = Math.floor(Math.random() * (maxStartCol + 1));
+  return [x, y];
+}
+
+export default function addEnemyData(player) {
+  const ships = [
+    new Ship("Carrier", 5),
+    new Ship("Battleship", 4),
+    new Ship("Cruiser", 3),
+    new Ship("Submarine", 3),
+    new Ship("Destroyer", 2),
+  ];
+
+  ships.forEach((ship) => {
+    let placed = false;
+    while (!placed) {
+      const coord = getRandomHorizontalCoord(
+        ship.getLength(),
+        player.gameBoard
+      );
+      placed = player.gameBoard.placeShip(ship, coord);
+    }
+  });
+}
