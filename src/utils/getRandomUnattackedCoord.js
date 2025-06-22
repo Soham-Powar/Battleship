c5f6b6350e359f34a947a1956707ce5a5c14e7b7
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-22 17:41:23 +0530

    Make the computer attack logic better by targeting nearby block first

--- src/utils/getRandomUnattackedCoord.js
+++ src/utils/getRandomUnattackedCoord.js
@@ -17,7 +17,45 @@ export default function getRandomUnattackedCoord(player) {
 
   player.gameBoard.missedShots.forEach((coord) => attackedCoords.push(coord));
 
-  // Filter out attacked coords
+  //Find hit positions of unsunk ships
+  const hitCoords = [];
+  player.gameBoard.ships.forEach((shipData) => {
+    if (!shipData.ship.isSunk()) {
+      shipData.attackedOn.forEach((coord) => hitCoords.push(coord));
+    }
+  });
+
+  //Find adjacent coords to above(hit) coords
+  const adjacentCoords = [];
+  const directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+
+  for (const [x, y] of hitCoords) {
+    for (const [dx, dy] of directions) {
+      const nx = x + dx;
+      const ny = y + dy;
+      if (
+        nx >= 0 &&
+        ny < 10 &&
+        ny >= 0 &&
+        ny < 10 &&
+        !attackedCoords.some(([ax, ay]) => ax === nx && ay === ny)
+      ) {
+        adjacentCoords.push([nx, ny]);
+      }
+    }
+  }
+
+  if (adjacentCoords.length > 0) {
+    const randomIndex = Math.floor(Math.random() * adjacentCoords.length);
+    return adjacentCoords[randomIndex];
+  }
+
+  // If no coords in proximity - Filter out attacked coords
   const unattackedCoords = allCoords.filter(
     ([x, y]) => !attackedCoords.some(([ax, ay]) => ax === x && ay === y)
   );
